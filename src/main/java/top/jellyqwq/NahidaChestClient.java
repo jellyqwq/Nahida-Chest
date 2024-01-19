3c81702c65c73503b2a0ea8af156ca169f1f5816
SweetJellyQwQ <577430840@qq.com>
2024-01-19 11:49:23 +0800

    Initial commit

---
+++ src/main/java/top/jellyqwq/NahidaChestClient.java
@@ -0,0 +1,10 @@
+package top.jellyqwq;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NahidaChestClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/top/jellyqwq/NahidachestDataGenerator.java
@@ -0,0 +1,11 @@
+package top.jellyqwq;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class NahidachestDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
